45e1f64d0493a3d8cb3efb86f843727b52da774c
konsan1101 <konchan@gold.ocn.ne.jp>
2019-07-19 22:19:04 +0900

    backup

--- screencapture/test_screen_ffmpeg.py
+++ screencapture/test_screen_ffmpeg.py
@@ -7,6 +7,7 @@ import time
 
 ffmpeg = subprocess.Popen(['ffmpeg', '-f', 'gdigrab', '-i', 'desktop', '-r', '5', 'desktop.mp4', ])
 time.sleep(10.00)
+#ffmpeg.send_signal(signal.SIGINT)
 ffmpeg.send_signal(signal.CTRL_C_EVENT)
 ffmpeg.terminate(10.00)
 ffmpeg = None
