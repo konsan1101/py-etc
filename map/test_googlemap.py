e46a746ceb3c9fd92379b94c69797887515555a1
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-07-24 19:13:32 +0900

    Update test_googlemap.py

--- map/test_googlemap.py
+++ map/test_googlemap.py
@@ -13,8 +13,9 @@
 
 #http://vivo-design.blogspot.com/2011/06/google-map-apipythongooglemaps.html
 
-from googlemaps import GoogleMaps
-gmaps = GoogleMaps()
+#from googlemaps import GoogleMaps
+import googlemaps as gm
+gmaps = gm.googlemaps()
 address = u'岡山市'
 lat, lng = gmaps.address_to_latlng(address)
 print(lat, lng)
