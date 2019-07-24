725e6e7c4bb049cfb2bf6576e8d48e5b896e622a
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-07-24 18:53:13 +0900

    backup

---
+++ map/test_googlemap.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#https://www.robotech-note.com/entry/2016/12/21/213024
+
+#from pygeocoder import Geocoder
+#address = u'国会議事堂'
+#results = Geocoder.geocode(address)
+#print(results[0].coordinates)
+
+#result = Geocoder.reverse_geocode(*results.coordinates, language="ja")
+#print(result)
+
+#http://vivo-design.blogspot.com/2011/06/google-map-apipythongooglemaps.html
+
+from googlemaps import GoogleMaps
+gmaps = GoogleMaps()
+address = u'岡山市'
+lat, lng = gmaps.address_to_latlng(address)
+print(lat, lng)
+
+
+
