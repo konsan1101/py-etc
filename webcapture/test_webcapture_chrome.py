5ffa65823ad9b972944c447243d7818b9d5f3375
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-07-25 08:33:04 +0900

    backup

--- webcapture/test_webcapture_chrome.py
+++ webcapture/test_webcapture_chrome.py
@@ -5,29 +5,40 @@
 #https://www.seleniumqref.com/api/webdriver_gyaku.html
 
 from selenium.webdriver import Chrome, ChromeOptions
+import sys
 import time
 
-# ヘッドレスモードオプション
-options = ChromeOptions()
-options.add_argument('-headless')
+url='https://www.a-zip.co.jp/'
+img='test_chrome.png'
 
-# ChromeのWebDriver作成
-driver = Chrome(options=options)
+if __name__ == '__main__':
+    if (len(sys.argv) >= 2):
+        url = str(sys.argv[1])
+    if (len(sys.argv) >= 3):
+        img = str(sys.argv[2])
 
-# URLを開く
-driver.get('https://www.a-zip.co.jp/')
+    # ヘッドレスモードオプション
+    options = ChromeOptions()
+    options.add_argument('-headless')
 
-# 読み込み待機
-time.sleep(5)
+    # ChromeのWebDriver作成
+    driver = Chrome(options=options)
 
-# ウィンドウサイズとズームを設定
-driver.set_window_size(1280, 3000)
-driver.execute_script("document.body.style.zoom='90%'")
+    # ウィンドウサイズとズームを設定
+    driver.set_window_size(1920, 9999)
+    driver.execute_script("document.body.style.zoom='100%'")
 
-# webページショット
-driver.save_screenshot('test_chrome.png')
+    # URLを開く
+    driver.get(url)
 
-# プラウザを閉じる
-driver.quit()
+    # 読み込み待機
+    time.sleep(5)
+
+    # webページショット
+    #driver.get_screenshot_as_file(img)
+    driver.save_screenshot(img)
+
+    # プラウザを閉じる
+    driver.quit()
 
 
