ba6adf0b1a71a621c82ab811962f8d5da0a6c345
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-08-03 13:36:26 +0900

    Update test_ffplay.py

--- ffplay/test_ffplay.py
+++ ffplay/test_ffplay.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import subprocess
+
 if __name__ == '__main__':
 
-    ffplay = subprocess.Popen(['ffmpeg', '-i', 'test_input.flv', \
-        '-vf', 'select=gt(scene\,0.1), scale=0:0,showinfo', \
-        '-vsync', 'vfr', 'temp/%04d.jpg', \
+    #ffplay -i test_input.flv -volume 100 -window_title "test_input.flv" -noborder -autoexit -fs
+    #ffplay -i test_input.flv -volume 100 -window_title "test_input.flv" -noborder -autoexit -x 320 -y 240
+
+    ffplay = subprocess.Popen(['ffplay', '-i', 'test_input.flv', \
+                                '-volume', '100', \
+                                '-window_title', 'test_input.flv', \
+                                '-noborder', '-autoexit', \
+                                '-x', '320', '-y', '240', \
         #], )
         ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, )
 
