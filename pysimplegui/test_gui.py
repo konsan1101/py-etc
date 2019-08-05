d0dfaa1ca5127cfd21fa713305b5624b441e5104
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-08-05 12:15:24 +0900

    Update test_gui.py

--- pysimplegui/test_gui.py
+++ pysimplegui/test_gui.py
@@ -8,14 +8,14 @@ import PySimpleGUI as sg
 # Very basic window.  Return values using auto numbered keys
 
 layout = [
-    [sg.Text('Please enter your Name, Address, Phone')],
-    [sg.Text('Name', size=(15, 1)), sg.InputText()],
-    [sg.Text('Address', size=(15, 1)), sg.InputText()],
-    [sg.Text('Phone', size=(15, 1)), sg.InputText()],
+    [sg.Text(u'名前、住所、電話を入力してね。^^;')],
+    [sg.Text(u'お名前', size=(15, 1)), sg.InputText()],
+    [sg.Text(u'ご住所', size=(15, 1)), sg.InputText()],
+    [sg.Text(u'電話番号', size=(15, 1)), sg.InputText()],
     [sg.Submit(), sg.Cancel()]
 ]
 
-window = sg.Window('Simple data entry window', layout)
+window = sg.Window(u'シンプル入力', layout)
 event, values = window.Read()
 window.Close()
 print(event, values[0], values[1], values[2])    # the input data looks like a simple list when auto numbered
