6d9c52fe8fc69f919c93f90d7b94ffd6b6dc39e4
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2019-08-30 12:58:13 +0900

    backup

--- icon/test_icon.py
+++ icon/test_icon.py
@@ -3,8 +3,18 @@
 
 from PIL import Image
 
-img = Image.open('A-ZiP.png')
+# white color to trans
+img = Image.open('input.png')
+alpha = img.split()[3]
+img = img.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
+mask = Image.eval(alpha, lambda a: 255 if a <=128 else 0)
+img.paste(255, mask)
+img.save('output_gif.gif', transparency=0)
+
+# gif to icon
 icon_sizes =[(16,16),(24,24),(32,32),(48,48),(64,64),(128,128),(256,256),]
-img.save('A-ZiP.ico', size=icon_sizes)
+
+img = Image.open('output_gif.gif')
+img.save('output_icon.ico', size=icon_sizes, )
 
 
