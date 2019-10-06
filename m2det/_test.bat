b8fc278f3bc52abf89c456ff1fefeb0bc9a8f42e
konsan1101 <konchan@gold.ocn.ne.jp>
2019-10-06 23:55:22 +0900

    backup

---
+++ m2det/_test.bat
@@ -0,0 +1,7 @@
+python demo.py -c=configs/m2det512_vgg.py -m=weights/m2det512_vgg.pth --show
+
+pause
+
+python demo.py -c=configs/m2det512_vgg.py -m=weights/m2det512_vgg.pth --show --cam=0
+
+pause
