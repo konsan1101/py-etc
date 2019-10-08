da1401fe1411de823ecbd0db0e383fda99313b3d
konsan1101 <konchan@gold.ocn.ne.jp>
2019-10-08 22:46:51 +0900

    backup

--- cv2dnn/opencv_yolov3.py
+++ cv2dnn/opencv_yolov3.py
@@ -7,14 +7,13 @@ import cv2
 import numpy as np
 import codecs
 
-threshold_score = 0.5
-threshold_nms   = 0.4
 file_config  = 'yolov3/yolov3-tiny.cfg'
 file_weights = 'yolov3/yolov3-tiny.weights'
 file_labels  = 'yolov3/coco-labels.txt'
-
 #file_config  = 'yolov3/yolov3.cfg'
 #file_weights = 'yolov3/yolov3.weights'
+threshold_score = 0.5
+threshold_nms   = 0.4
 
 # モデルの読み込み
 model = cv2.dnn.readNetFromDarknet(file_config, file_weights)
@@ -66,6 +65,10 @@ model.setInput(blob)
 #output = model.forwarscore
 output = model.forward(layer_names)
 
+pass_classids = []
+pass_scores   = []
+pass_boxes    = []
+
 # ループ
 for detections in output:
 
@@ -77,9 +80,6 @@ for detections in output:
         classid = np.argmax(scores)
 
         # 予測確率がthreshold_score以上を取り出す。
-        pass_classids = []
-        pass_scores   = []
-        pass_boxes    = []
         score = scores[classid]
         if (score >= threshold_score):
 
@@ -92,42 +92,42 @@ for detections in output:
             top    = int(center_y - b_height/2)
             width  = int(b_width)
             height = int(b_height)
-            print('ok')
 
             # 変数に取り出す。
-            pass_classids.append(classid)
-            pass_scores.append(float(score))
-            pass_boxes.append([left, top, width, height])
+            if (width>=10) and ((height)>=10):
+                pass_classids.append(classid)
+                pass_scores.append(float(score))
+                pass_boxes.append([left, top, width, height])
     
-        # 重複した領域を排除した内容を利用する。
-        indices = cv2.dnn.NMSBoxes(pass_boxes, pass_scores, threshold_score, threshold_nms)
-        for i in indices:
-            i = i[0]
-            classid = pass_classids[i]
-            score   = pass_scores[i]
-            box     = pass_boxes[i]
-            left    = box[0]
-            top     = box[1]
-            width   = box[2]
-            height  = box[3]
+# 重複した領域を排除した内容を利用する。
+indices = cv2.dnn.NMSBoxes(pass_boxes, pass_scores, float(threshold_score), float(threshold_nms))
+for i in indices:
+    i = i[0]
+    classid = pass_classids[i]
+    score   = pass_scores[i]
+    box     = pass_boxes[i]
+    left    = box[0]
+    top     = box[1]
+    width   = box[2]
+    height  = box[3]
 
-            # クラス名を取り出す。
-            class_name  = classNames[classid]
-            class_color = [ int(c) for c in classColors[classid] ]
-            label       = class_name + ' {0:.2f}'.format(score)
+    # クラス名を取り出す。
+    class_name  = classNames[classid]
+    class_color = [ int(c) for c in classColors[classid] ]
+    label       = class_name + ' {0:.2f}'.format(score)
 
-            # 検出された物体の名前を表示
-            print(label)
+    # 検出された物体の名前を表示
+    print(label)
 
-            # (画像、開始座標、終了座標、色、線の太さ)を指定
-            cv2.rectangle(out_image, (left, top), (left+width, top+height), class_color, thickness=2)
+    # (画像、開始座標、終了座標、色、線の太さ)を指定
+    cv2.rectangle(out_image, (left, top), (left+width, top+height), class_color, thickness=2)
 
-            # (画像、文字列、開始座標、フォント、文字サイズ、色)を指定
-            t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1 , 1)[0]
-            x = left + t_size[0] + 3
-            y = top + height - t_size[1] - 4
-            cv2.rectangle(out_image, (left, y), (x, top + height), class_color, -1)
-            cv2.putText(out_image, label, (left, y + t_size[1] + 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
+    # (画像、文字列、開始座標、フォント、文字サイズ、色)を指定
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1 , 1)[0]
+    x = left + t_size[0] + 3
+    y = top + t_size[1] + 4
+    cv2.rectangle(out_image, (left, top), (x, y), class_color, -1)
+    cv2.putText(out_image, label, (left, top + t_size[1] + 1), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1)
 
 # 出力画像復元
 if (image_width > image_height):
