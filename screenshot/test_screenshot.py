b8fb35483e1660e146b3a7b79a30875f0f20a13b
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2020-02-13 17:25:26 +0900

    backup

--- screenshot/test_screenshot.py
+++ screenshot/test_screenshot.py
@@ -7,10 +7,15 @@ s = pyautogui.screenshot()
 s.save('test_shot1.jpg')
 
 import ctypes
+
 user32 = ctypes.windll.user32
 x,y  = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
-
 s = pyautogui.screenshot(region=(0,0,x,y))
 s.save('test_shot2.jpg')
+
+from PIL import ImageGrab
+
+img = ImageGrab.grab(bbox=(0, 0, x, y))
+img.save('test_shot3.jpg')
 
 
