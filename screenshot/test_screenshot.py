7e6d09fd05bf1910130929a60a1bd958ee974cd9
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2020-02-13 17:13:04 +0900

    backup

--- screenshot/test_screenshot.py
+++ screenshot/test_screenshot.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pyautogui         
+
 s = pyautogui.screenshot()
-s.save('test_screenshot.jpg')
+s.save('test_shot1.jpg')
+
+import ctypes
+user32 = ctypes.windll.user32
+x,y  = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
+
+s = pyautogui.screenshot(region=(0,0,x,y))
+s.save('test_shot2.jpg')
+
+
