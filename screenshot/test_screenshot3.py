d93fd6e00dc24f912058811cfcb70b60456ecdb5
konsan1101 <konchan@gold.ocn.ne.jp>
2020-02-14 23:01:15 +0900

    backup

--- screenshot/test_screenshot3.py
+++ screenshot/test_screenshot3.py
@@ -3,50 +3,55 @@
 
 # http://e-yuki67.hatenablog.com/entry/2017/02/12/152759
 
-
-import ctypes
-user32 = ctypes.windll.user32
-SCREEN_WIDTH  = user32.GetSystemMetrics(78)
-SCREEN_HEIGHT = user32.GetSystemMetrics(79)
-SCREEN_SCALING_FACTOR = 1.0
-
-import win32gui
-import win32ui
-import win32con
-from PIL import Image
+import os
 import numpy as np
 import cv2
 
-#SCREEN_WIDTH = 1920
-#SCREEN_HEIGHT = 1080
-#SCREEN_SCALING_FACTOR = 1.0
+if (os.name == 'nt'):
+    import ctypes
+    user32 = ctypes.windll.user32
+    SCREEN_WIDTH  = user32.GetSystemMetrics(78)
+    SCREEN_HEIGHT = user32.GetSystemMetrics(79)
 
-def nt_screenshot():
-    try:
+    import win32gui
+    import win32ui
+    import win32con
+    from PIL import Image
+
+class win_shot_class:
+
+    def __init__(self, ):
+        pass
+    
+    def screenshot(self, ):
         window = win32gui.GetDesktopWindow()
         window_dc = win32ui.CreateDCFromHandle(win32gui.GetWindowDC(window))
         compatible_dc = window_dc.CreateCompatibleDC()
-        width = SCREEN_WIDTH
-        height = SCREEN_HEIGHT
+
         bmp = win32ui.CreateBitmap()
-        bmp.CreateCompatibleBitmap(window_dc, width, height)
+        bmp.CreateCompatibleBitmap(window_dc, SCREEN_WIDTH, SCREEN_HEIGHT)
         compatible_dc.SelectObject(bmp)
-        compatible_dc.BitBlt((0, 0), (width, height), window_dc, (0, 0), win32con.SRCCOPY)
-        pil_img = Image.frombuffer('RGB', (width, height), bmp.GetBitmapBits(True), 'raw', 'BGRX', 0, 1)
-        cv2_img = np.array(pil_img, dtype=np.uint8)
-        cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_RGB2BGR)
-        return cv2_img
-    except:
-        return None
+        compatible_dc.BitBlt((0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), window_dc, (0, 0), win32con.SRCCOPY)
+        pil_img = Image.frombuffer('RGB', (SCREEN_WIDTH, SCREEN_HEIGHT), bmp.GetBitmapBits(True), 'raw', 'BGRX', 0, 1)
+
+        window = None
+        window_dc = None
+        compatible_dc = None
+        bmp = None
+        return pil_img
 
 
 
 if __name__ == '__main__':
 
+    win_shot = win_shot_class()
+    frm = win_shot.screenshot()
+    frame = np.asarray(frm, dtype=np.uint8)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-    img = screenshot()
-    cv2.imshow("", img)
+    cv2.imshow("", frame)
     cv2.waitKey(0)
+
     cv2.destroyAllWindows()
 
 
