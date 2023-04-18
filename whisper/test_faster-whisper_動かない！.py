e5d1626e4e3eca426c8220d1f3a81bd9624c89e9
Mitsuo KONDOU <konchan@gold.ocn.ne.jp>
2023-04-19 05:29:15 +0900

    backup

---
+++ whisper/test_faster-whisper_動かない！.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://github.com/guillaumekln/faster-whisper
+
+from faster_whisper import WhisperModel
+
+model_size = "large-v2"
+
+# Run on GPU with FP16
+#model = WhisperModel(model_size, device="cpu", compute_type="float16")
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+# or run on GPU with INT8
+# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+# or run on CPU with INT8
+# model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+segments, info = model.transcribe("temp_pfm/temp4_output.wav", beam_size=5)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+
